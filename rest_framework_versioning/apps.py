84c96b818ca37bc3ae2b30c72b236cc8340f937a
binnev <byntje@gmail.com>
2022-06-16 15:00:46 +0200

    startapp rest_framework_versioning

---
+++ rest_framework_versioning/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RestFrameworkVersioningConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'rest_framework_versioning'
