850f06768efd0716f246b2dd1830841f46788513
binnev <byntje@gmail.com>
2022-07-20 13:39:09 +0200

    added test for viewset

--- tests/tests/test_views.py
+++ tests/tests/test_views.py
@@ -1,6 +1,15 @@
+from rest_framework.test import APIRequestFactory
+
 from drf_versioning.views import VersionViewSet
-from tests.versions import VERSIONS
 
 
-def test_version_viewset_get_queryset():
-    assert VersionViewSet().get_queryset() == VERSIONS
+def test_my_version():
+    factory = APIRequestFactory()
+    request = factory.get("", HTTP_ACCEPT="application/json; version=2.0.0")
+    view = VersionViewSet.as_view(actions={"get": "my_version"})
+    response = view(request)
+    assert response.status_code == 200
+    assert response.data == {
+        "version": "2.0.0",
+        "notes": ["Added Thing model."],
+    }
