f7a5a3a916b6a5c5e8dd65b965c0695a8930cf01
binnev <byntje@gmail.com>
2022-07-20 13:06:18 +0200

    made VERSIONS a tuple so it's immutable.

--- tests/versions.py
+++ tests/versions.py
@@ -5,8 +5,8 @@ VERSION_1_0_0 = Version("1.0.0", notes=["Initial version"])
 VERSION_2_0_0 = Version("2.0.0", notes=["Added Thing model."])
 VERSION_2_1_0 = Version("2.1.0", transforms=[transforms.ThingTransformAddNumber])
 
-VERSIONS = [
+VERSIONS = (
     VERSION_1_0_0,
     VERSION_2_0_0,
     VERSION_2_1_0,
-]
+)
