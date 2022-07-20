886416b7a804e13baadfb0586b77b1486f25d716
binnev <byntje@gmail.com>
2022-07-20 13:19:59 +0200

    added settings test

--- tests/tests/test_settings.py
+++ tests/tests/test_settings.py
@@ -1,6 +1,16 @@
-from drf_versioning.settings import versioning_settings
+from drf_versioning import settings
 from tests.versions import VERSIONS
 
 
 def test_settings():
-    assert versioning_settings.VERSION_LIST == VERSIONS
+    s = settings.VersioningSettings(
+        user_settings={"DEFAULT_VERSION": "6.9"},
+        defaults=settings.DEFAULTS,
+        import_strings=settings.IMPORT_STRINGS,
+    )
+    assert s.DEFAULT_VERSION == "6.9"
+    assert s.VERSION_LIST == []
+
+
+def test_settings_loads_version_list():
+    assert settings.versioning_settings.VERSION_LIST == VERSIONS
