ba38cbc3355df3b6ed347bdc308854fd48103c83
binnev <byntje@gmail.com>
2022-07-20 11:30:31 +0200

    Changed middleware to return get_default() instead of get_latest(). Added test.

--- drf_versioning/middleware.py
+++ drf_versioning/middleware.py
@@ -6,5 +6,6 @@ from drf_versioning.version import Version
 class AcceptHeaderVersioning(_AcceptHeaderVersioning):
     def determine_version(self, request, *args, **kwargs):
         version = super().determine_version(request, *args, **kwargs)
-        version = Version.get(version) if version else Version.get_latest()
+        if version is None:
+            return Version.get_default().base_version
         return version
---
+++ tests/tests/test_middleware.py
@@ -0,0 +1,26 @@
+from unittest.mock import patch
+
+import pytest
+
+from drf_versioning.middleware import AcceptHeaderVersioning
+from drf_versioning.version import Version
+
+
+@pytest.mark.parametrize(
+    "super_version, expected_version",
+    [
+        ("2.0.0", "2.0.0"),  # version passed in request
+        (None, "666.69.420"),  # no version in request -> use default
+    ],
+)
+@patch("drf_versioning.version.base.Version.get_default", return_value=Version("666.69.420"))
+@patch("rest_framework.versioning.AcceptHeaderVersioning.determine_version")
+def test_accept_header_versioning(
+    mock, mock_get_default, super_version, expected_version, patch_settings
+):
+    mock.return_value = super_version
+    version = AcceptHeaderVersioning().determine_version(...)
+
+    assert isinstance(version, str)
+    assert version == expected_version
+    mock.assert_called_with(...)
