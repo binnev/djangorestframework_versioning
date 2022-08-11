3c2c70e99ee4d86d433ad28644904787e6eba12f
binnev <byntje@gmail.com>
2022-08-11 17:35:53 +0200

    Updated ThingTransformAddNumber

--- tests/versions.py
+++ tests/versions.py
@@ -1,10 +1,9 @@
 from drf_versioning.version import Version
-from . import transforms
 
 VERSION_0_0_1 = Version("0.0.1", notes=["Pre-release"])
 VERSION_1_0_0 = Version("1.0.0", notes=["Initial version"])
 VERSION_2_0_0 = Version("2.0.0", notes=["Added Thing model."])
-VERSION_2_1_0 = Version("2.1.0", transforms=[transforms.ThingTransformAddNumber])
+VERSION_2_1_0 = Version("2.1.0")
 VERSION_2_2_0 = Version("2.2.0", notes=["Did nothing."])
 
 VERSIONS = (
