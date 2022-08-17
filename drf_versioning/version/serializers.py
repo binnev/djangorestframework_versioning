88cab4f50d5e928cb8b96d1c8034ee388d09d6fc
binnev <byntje@gmail.com>
2022-08-17 11:52:24 +0200

    Made version serialization a lot slicker.

--- drf_versioning/version/serializers.py
+++ drf_versioning/version/serializers.py
@@ -1,29 +1,64 @@
+import re
+
 from rest_framework import serializers
+
+from drf_versioning.transform import Transform
+
+"""
+This is how I'd want the serialized Versions to look:
+[
+    {
+        "version": "1.0.0",
+        "views": [
+            "Introduced /thing/ endpoint with actions:
+                create:     POST /thing/
+                retrieve:   GET /thing/{id}/"
+        ]
+    },
+    {
+        "version": "2.0.0",
+        "views": [
+            "Introduced /thing/ actions:
+                list:   GET /thing/"
+        ]
+    },
+    {
+        "version": "2.1.0",
+        "views": [
+            "Introduced /thing/ actions:
+                get_name:   GET /thing/{id}/get_name"
+        ]
+    },
+]
+"""
 
 
 class TransformSerializer(serializers.Serializer):
-    description = serializers.CharField(source="*")
+    def to_representation(self, obj: Transform):
+        return obj.description
 
 
-class AddViewSetActionSerializer(serializers.Serializer):
-    def to_representation(self, instance):
-        return f"Introduced {instance}"
+class ViewSetMethodSerializer(serializers.Serializer):
+    def to_representation(self, obj):
+        match = re.match("<function (.*) at (.*)>", str(obj))
+        method_name, _ = match.groups()
+        return method_name
 
 
-class RemoveViewSetActionSerializer(serializers.Serializer):
-    def to_representation(self, instance):
-        return f"Removed {instance}"
+class ViewSetSerializer(serializers.Serializer):
+    def to_representation(self, obj):
+        return obj.__name__
 
 
 class ViewMethodSerializer(serializers.Serializer):
-    viewsets_introduced = AddViewSetActionSerializer(many=True)
-    viewsets_removed = RemoveViewSetActionSerializer(many=True)
-    view_methods_introduced = AddViewSetActionSerializer(many=True)
-    view_methods_removed = RemoveViewSetActionSerializer(many=True)
+    endpoints_introduced = ViewSetSerializer(source="viewsets_introduced", many=True)
+    endpoints_removed = ViewSetSerializer(source="viewsets_removed", many=True)
+    actions_introduced = ViewSetMethodSerializer(source="view_methods_introduced", many=True)
+    actions_removed = ViewSetMethodSerializer(source="view_methods_removed", many=True)
 
 
 class VersionSerializer(serializers.Serializer):
     version = serializers.CharField(source="base_version")
     notes = serializers.ListField()
-    transforms = TransformSerializer(many=True)
+    models = TransformSerializer(source="transforms", many=True)
     views = ViewMethodSerializer(source="*")
--- tests/transforms.py
+++ tests/transforms.py
@@ -1,6 +1,7 @@
 from drf_versioning.transform import AddField
 from tests import versions
 
+
 class ThingTransformAddNumber(AddField):
     field_name = "number"
     description = "Added Thing.number field."
