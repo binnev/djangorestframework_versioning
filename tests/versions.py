68e341b5fc271228e3a34093f1c858845140c9b4
binnev <byntje@gmail.com>
2022-09-13 12:20:51 +0200

    Removed lies from version notes.

--- tests/versions.py
+++ tests/versions.py
@@ -4,7 +4,7 @@ VERSION_0_0_1 = Version("0.0.1", notes=["Pre-release"])
 VERSION_1_0_0 = Version("1.0.0", notes=["Initial version"])
 VERSION_2_0_0 = Version("2.0.0", notes=["Added Thing model."])
 VERSION_2_1_0 = Version("2.1.0")
-VERSION_2_2_0 = Version("2.2.0", notes=["Did nothing."])
+VERSION_2_2_0 = Version("2.2.0")
 
 VERSIONS = (
     VERSION_0_0_1,
