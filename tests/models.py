ac30acc21c1b0e5826b3f79e794bb26db851266d
binnev <byntje@gmail.com>
2022-09-13 12:20:25 +0200

    Added 2 new fields to Thing model

--- tests/models.py
+++ tests/models.py
@@ -1,6 +1,14 @@
 from django.db import models
+from django.utils import timezone
+
+
+class ThingStatus(models.TextChoices):
+    OK = "OK"
+    NOT_OK = "NOT_OK"
 
 
 class Thing(models.Model):
     name = models.CharField(max_length=50)
     number = models.IntegerField(default=0)
+    status = models.CharField(max_length=20, choices=ThingStatus.choices, default=ThingStatus.OK)
+    date_updated = models.DateTimeField(default=timezone.now)
